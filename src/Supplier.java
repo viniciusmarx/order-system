71802e5aaa34db18f58a2097864a05ec1090e6f9
Éric Salvi <ericsalvi67@hotmail.com>
2025-05-07 22:16:29 -0300

    ✨Refactor Supplier constructor to include CNPJ parameter

--- src/Supplier.java
+++ src/Supplier.java
@@ -2,13 +2,13 @@ public class Supplier extends User {
     private String cnpj;
     private String description;
 
-    public Supplier(String name, String email, String phoneNumber, Address address, String description){
+    public Supplier(String name, String email, String phoneNumber, Address address, String cnpj, String description){
         super(name, email, phoneNumber, address);
         this.cnpj = cnpj;
         this.description = description;
     }
 
-    public Supplier(String name, String email, String phoneNumber, Address address){
+    public Supplier(String name, String email, String phoneNumber, Address address, String cnpj){
         super(name, email, phoneNumber, address);
         this.cnpj = cnpj;
     }
